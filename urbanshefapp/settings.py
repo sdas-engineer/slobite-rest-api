e6dba974cb79ef49aafe7db28265c937a8781f6a
MySecoundLanguage <mmamunism@gmail.com>
2021-04-21 15:15:44 +0600

    added http verv

--- urbanshefapp/settings.py
+++ urbanshefapp/settings.py
@@ -29,7 +29,7 @@ DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 
-SECURE_SSL_REDIRECT = True
+# SECURE_SSL_REDIRECT = True
 
 
 # Application definition
