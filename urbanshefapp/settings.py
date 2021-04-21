142a13d8373bd2ded6bd32894657dc1be8beaab6
MySecoundLanguage <mmamunism@gmail.com>
2021-04-21 17:42:59 +0600

    waji

--- urbanshefapp/settings.py
+++ urbanshefapp/settings.py
@@ -29,13 +29,7 @@ DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
 
-SECURE_SSL_REDIRECT = True
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-SESSION_COOKIE_SECURE = True
-
-CSRF_COOKIE_SECURE = True
 
 
 
