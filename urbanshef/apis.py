918f4f57cddd506d41a0da98abc6b699ac14c2a4
Urbanshef Developers <developers.urbanshef@gmail.com>
2021-05-11 23:49:10 +0000

    Removed delivery instructions from order creation

--- urbanshef/apis.py
+++ urbanshef/apis.py
@@ -162,7 +162,6 @@ class CustomerAddAPIView(generics.CreateAPIView):
                     # tax=(order_total + 3) * 0.2,
                     phone=request.POST["phone"],
                     delivery_charge=delivery_charge,
-                    delivery_instructions=request.POST('delivery_instructions'),
                     service_charge=request.POST.get('service_charge')
                 )
 
