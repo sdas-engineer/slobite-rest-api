91f7295449c7f7523cdb64a6de878741f9c0a158
Urbanshef Developers <developers.urbanshef@gmail.com>
2021-05-17 21:34:11 +0000

    Updated allowed hosts

--- urbanshefapp/settings.py
+++ urbanshefapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', True)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
+ALLOWED_HOSTS = ['urbanshef.com', 'www.urbanshef.com']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # SECURE_SSL_REDIRECT = True
