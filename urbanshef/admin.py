f4c5cf213305358cd6beb226e6b1651e93186b66
Nasir <nasirkhancse@gmail.com>
2021-05-27 02:41:55 +0600

    Admin backend is customized

--- urbanshef/admin.py
+++ urbanshef/admin.py
@@ -3,21 +3,106 @@ from django.contrib import admin
 # Register your models here.
 from urbanshef.models import Chef, Customer, Driver, Meal, Order, OrderDetails, Review, Coupon
 
-admin.site.register(Chef)
-admin.site.register(Customer)
-admin.site.register(Driver)
-admin.site.register(Meal)
+
+class ChefModel(admin.ModelAdmin):
+    class Meta:
+        model = Chef
+
+    list_display = ['__str__', 'phone', 'available', 'disabled_by_admin', 'gender', 'cuisine']
+    list_filter = ['available', 'disabled_by_admin', 'gender', 'cuisine']
+    list_display_links = ['__str__']
+    search_fields = ['phone', 'name']
+    list_per_page = 25
+
+
+admin.site.register(Chef, ChefModel)
+
+
+class CustomerModel(admin.ModelAdmin):
+    class Meta:
+        model = Customer
+
+    list_display = ['__str__', 'phone']
+    list_display_links = ['__str__']
+    search_fields = ['phone', 'name']
+    list_per_page = 25
+
+
+admin.site.register(Customer, CustomerModel)
+
+
+class DriverModel(admin.ModelAdmin):
+    class Meta:
+        model = Driver
+
+    list_display = ['__str__', 'phone']
+    list_display_links = ['__str__']
+    search_fields = ['phone', 'name']
+    list_per_page = 25
+
+
+admin.site.register(Driver, DriverModel)
+
+
+class MealModel(admin.ModelAdmin):
+    class Meta:
+        model = Meal
+
+    list_display = ['__str__', 'chef', 'price', 'food_type', 'allergen', 'cuisine', 'availability']
+    list_display_links = ['__str__']
+    search_fields = ['name', 'food_type', 'allergen', 'cuisine']
+    list_filter = ['availability', 'food_type', 'allergen', 'cuisine', ]
+    list_per_page = 25
+
+
+admin.site.register(Meal, MealModel)
 
 
 class OrderModel(admin.ModelAdmin):
     class Meta:
         model = Order
 
-    list_display = ['__str__']
-    list_filter = ['created_at']
+    list_display = ['__str__', 'chef', 'customer', 'total', 'status']
+    list_filter = ['created_at', 'status']
+    list_display_links = ['__str__']
+    list_per_page = 25
 
 
 admin.site.register(Order, OrderModel)
-admin.site.register(OrderDetails)
-admin.site.register(Review)
-admin.site.register(Coupon)
+
+
+class OrderDetailsModel(admin.ModelAdmin):
+    class Meta:
+        model = OrderDetails
+
+    list_display = ['__str__', 'meal', 'quantity', 'delivery_charge', 'sub_total']
+    list_per_page = 25
+
+
+admin.site.register(OrderDetails, OrderDetailsModel)
+
+
+class ReviewModel(admin.ModelAdmin):
+    class Meta:
+        model = Review
+
+    list_display = ['__str__', 'chef', 'customer', 'rating']
+    list_filter = ['rating']
+    list_per_page = 25
+    search_fields = ['chef', 'customer']
+
+
+admin.site.register(Review, ReviewModel)
+
+
+class CouponModel(admin.ModelAdmin):
+    class Meta:
+        model = Coupon
+
+    list_display = ['__str__', 'valid_from', 'valid_to', 'active']
+    list_filter = ['valid_from', 'valid_to', 'active']
+    list_per_page = 25
+    search_fields = ['code']
+
+
+admin.site.register(Coupon, CouponModel)
