cc265e812353c885f9983ae4e8bfe2bad6cf0bb0
Nasir <nasirkhancse@gmail.com>
2021-05-27 03:02:14 +0600

    Update swagger ui

--- urbanshefapp/urls.py
+++ urbanshefapp/urls.py
@@ -36,6 +36,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
+    url='https://urbanshef.com'
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
